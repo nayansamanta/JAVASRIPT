b04e7aba62b1720863e68ff52e63cb19c2d1eea9
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-08 18:36:45 +0000

    datatype added

---
+++ 01_class/datatype.js
@@ -0,0 +1,24 @@
+"use strict" ; // treat all js code as a newer version 
+
+// JavaScript is a powerful programming language that can add interactivity to a website. It was invented by Brendan Eich.
+
+// alert(3+3)  // we are using node js here not browser so simple alert not not working here it is applicable for browser 
+
+console.log(3+3);  // 6 
+
+let name = "nayan"
+let age = 20
+let IsloggIn = true 
+
+// number 
+// bigint 
+// string 
+// boolean = ture/false 
+// null is a standalone value in js and it is a object 
+// undefined  :  abhi tak value assigned nhi hua 
+// symbol => unique 
+
+// object 
+console.log(typeof "nayan");
+console.log(typeof undefined);  // undefined 
+console.log(typeof null);   // object 
