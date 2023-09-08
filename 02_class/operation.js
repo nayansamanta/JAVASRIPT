702d7c5115778e7eb73c1f2b043823d0d05b0175
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-08 20:19:09 +0000

    convertion and operation done

---
+++ 02_class/operation.js
@@ -0,0 +1,13 @@
+// operations 
+
+let num = 3
+let negNum = -num
+//console.log(negNum)
+
+let str1 = "hello"
+let str2 = " nayan"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2 + 2)
+console.log( 1 + 2 + "2")
