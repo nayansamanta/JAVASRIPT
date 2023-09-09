37011c6403090521fe83a3a2b3c29967ad9de3a9
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-09 15:36:43 +0000

    comparisn

---
+++ 03_class/comparison.js
@@ -0,0 +1,6 @@
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);  // comparison convert null to a number , treating is as 0 
+
+console.log("2" == 2);
+console.log("2" === 2);  // compare value with datatype 
