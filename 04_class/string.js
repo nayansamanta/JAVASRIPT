38e00bd7b9bcaa5f80fe6a2b52cc0d521483ddcb
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-10 17:39:27 +0000

    string added

---
+++ 04_class/string.js
@@ -0,0 +1,19 @@
+const name = "nayan"
+let repocount = 20
+
+// console.log(name + repocount + "  ok")
+
+console.log(`hello i am ${name} and my repo count is ${repocount}`);   // string interpolation 
+
+const myName = new String('nayan')
+console.log(myName[0])
+console.log(myName.length);
+console.log(myName.toUpperCase());
+console.log(myName.charAt(2));
+console.log(myName.indexOf('y'));
+
+// read the document : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String  
+
+const newone = "    nayan    "
+console.log(newone);
+console.log(newone.trim());   // remove space 
