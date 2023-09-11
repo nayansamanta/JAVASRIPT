d713ceb27f41c2ffd433244aec3d9cfa6a759d26
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-11 20:28:28 +0000

    array

---
+++ SECTION_02/01_class/array.js
@@ -0,0 +1,25 @@
+// array 
+
+const array1 = [2 ,3 , 8,9,6,45]
+//console.log(array1);
+
+const arra2 = new Array(4,8,9,8,6,52)
+//console.log(arra2);
+
+// array methods 
+
+array1.push(99)  // add value 
+//console.log(array1)
+
+array1.pop()  // remove last element 
+//console.log(array1);
+
+// console.log(array1.includes(100)) // return true or false 
+// console.log(array1.indexOf(6))   // in which index 6 exits 
+
+const newarr = array1.join()  // convert array to string 
+// console.log(array1)
+// console.log(newarr);
+// console.log(typeof newarr);
+
+// slice and splice 
