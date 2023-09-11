51e76bf1247bc28df91cd7755e840414261f9c5f
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-11 16:40:22 +0000

    date and time

---
+++ SECTION_01/05_class/date_time.js
@@ -0,0 +1,23 @@
+// date 
+
+let mydate = new Date()
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());  // best 
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+
+// let newdate = new Date(2023 , 11 ,28)
+// let newdate = new Date("2023-01-14")  
+// console.log(newdate.toDateString()) 
+
+// timeStamp 
+
+
+//  let mytimeStamp = Date.now()
+//  console.log(mytimeStamp) 
+
+let date1 = new Date()
+console.log(date1.getDay());
+console.log(date1.getMonth() +1 );   // septembr 
