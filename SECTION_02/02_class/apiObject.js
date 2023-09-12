c9782288da79755372cd6e00cbe810207b5a3f92
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-12 21:35:03 +0000

    json object

---
+++ SECTION_02/02_class/apiObject.js
@@ -0,0 +1,10 @@
+const course ={
+    coursename: "javascript",
+    price:999,
+    teacher:"shradha ma'am "
+} 
+
+const {teacher} = course  // destructuring 
+console.log(teacher);  
+
+// https://jsonformatter.org/#google_vignette   // use this link 
