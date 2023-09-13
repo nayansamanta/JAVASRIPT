352aef318dae131ab99d32bbcc4594e4ea8b6cc4
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-13 16:58:09 +0000

    this

---
+++ SECTION_04/this.js
@@ -0,0 +1,15 @@
+// function cofee() {
+//     console.log(this)
+// }
+// cofee()
+
+
+// const chai =function() {
+//     let username = "nayan"
+//     console.log(this.username)
+// }
+// chai()
+
+
+// syntax of arrow function : ()=>{}
+
