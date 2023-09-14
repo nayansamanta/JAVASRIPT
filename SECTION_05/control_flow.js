be6c190913a19737d8eb39ba1a86a31119676385
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-14 10:07:30 +0000

    if else

---
+++ SECTION_05/control_flow.js
@@ -0,0 +1,12 @@
+// if 
+
+// if(2=="2"){
+//     console.log("executed");
+// }
+
+// const score =200
+// if (score>100) {
+//     const power = "fly"
+//     console.log("ur score is greater than 100");
+//     console.log(`you can ${power}`);
+// } 
