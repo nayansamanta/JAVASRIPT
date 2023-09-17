f8b6b7ce366f39843c2268faf74d4da5ee7b59f4
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-17 18:20:19 +0000

    if and if else statement

--- SECTION_05/control_flow.js
+++ SECTION_05/control_flow.js
@@ -10,3 +10,22 @@
 //     console.log("ur score is greater than 100");
 //     console.log(`you can ${power}`);
 // } 
+
+// implicit scope 
+
+const balance =5000;
+if(balance>500) console.log("u have sufficient balance ") 
+
+// and operator 
+const userlogedin = true 
+const debitcard =true 
+if (userlogedin && debitcard) {  // two condition have to true 
+    console.log("you can buy anything");
+}
+
+// or operator 
+const loggedinfromGoogle = true
+const loggesinfromEmail = false 
+if (loggedinfromGoogle||loggesinfromEmail) {  // if anyone is true then condition will be check otherwise not  
+    console.log("ok you can enter into this website ")
+}
