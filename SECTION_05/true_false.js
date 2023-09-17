b043bfec0532911a0107afdbe1b6759e7073ef82
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-17 18:53:13 +0000

    ok

---
+++ SECTION_05/true_false.js
@@ -0,0 +1,16 @@
+const email = "nayan@gmail.com"
+
+if(email){
+    console.log("got a email");
+}else{
+    console.log("dont got a email")
+}
+
+// empty string is treated as a false value 
+const email1 = ""
+
+if(email1){
+    console.log("got a email");
+}else{
+    console.log("dont got a email")
+}
