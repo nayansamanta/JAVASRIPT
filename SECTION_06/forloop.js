b9018c5087a2272cc1a492dcc813012cfae9b9ed
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-17 20:37:02 +0000

    for loop

---
+++ SECTION_06/forloop.js
@@ -0,0 +1,28 @@
+// for 
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    //console.log(element);
+    
+}
+
+// break and continoue 
+
+for (let i = 1; i <=10; i++) {
+
+    if(i==5){
+        console.log(`5 is detected`);
+        break;
+    }
+    console.log(`value of i is ${i}`);
+    
+}
+for (let i = 1; i <=10; i++) {
+
+    if(i==5){
+        console.log(`5 is detected`);
+        continue;
+    }
+    console.log(`value of i is ${i}`);
+    
+}
