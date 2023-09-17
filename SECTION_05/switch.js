2cd0973819e1694fad76f19724734d8625cb84af
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-17 18:43:45 +0000

    switch statement

---
+++ SECTION_05/switch.js
@@ -0,0 +1,43 @@
+// switch 
+
+// syntax 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// for copy and paste press = shift+alt+down arrow 
+
+const week =9
+switch (week) {
+    case 1:
+        console.log("monday")
+        break;
+    case 2:
+        console.log("Tuesday")
+        break;
+    case 3:
+        console.log("wednesday")
+        break;
+    case 4:
+        console.log("thrusday")
+        break;
+    case 5:
+        console.log("friday")
+        break;
+    case 6:
+        console.log("saturday")
+        break;
+    case 7:
+        console.log("sunday")
+        break;
+
+    default:
+        console.log("u eneter worng day no ")
+        break;
+}
