4584768e1a19adc0d439aaa5f41d7f3308876664
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-18 18:29:20 +0000

    while and do while

---
+++ SECTION_06/while.js
@@ -0,0 +1,22 @@
+// while loops 
+let index =0 
+while (index<=10) {
+   // console.log("hello ");
+    index++
+}
+
+let myarray =[ " batman" , "superman " , "flash"]
+let index1 =0
+while (index1<myarray.length) {
+   // console.log(`the name of super hero is ${myarray[index1]}`);
+    index1++
+}
+
+// do while 
+
+num=0
+do {
+    console.log(`number is ${num}`);
+    num++
+} while (num<=10);
+
