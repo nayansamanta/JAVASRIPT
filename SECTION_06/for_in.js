6ed4e4fffb42b4b5c2f4f6277e447cf39512254c
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-09-18 19:15:55 +0000

    for in loops

---
+++ SECTION_06/for_in.js
@@ -0,0 +1,18 @@
+// for in 
+
+const myobject = {
+    js : "javascript",
+    cpp: "c++",
+    rb:"ruby"
+}
+for (const key in myobject) {
+  //console.log(myobject[key]);
+}
+
+
+// array 
+
+const programing =[ "ruby", "java" , "c++" , "nodejs"]
+for (const key in programing) {
+    console.log(programing[key]);
+  }
